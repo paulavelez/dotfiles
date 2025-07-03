04667307b96de0f6c60bb284e802277b41ce17ca
paulavelez <paula.velez@rubiconmd.com>
2025-07-03 16:49:24 +0200

    first dotfile!

---
+++ wezterm/.wezterm.lua
@@ -0,0 +1,18 @@
+-- Pull in the wezterm API
+local wezterm = require 'wezterm'
+
+-- This table will hold the configuration.
+local config = {}
+
+-- In newer versions of wezterm, use the config_builder which will
+-- help provide clearer error messages
+if wezterm.config_builder then
+    config = wezterm.config_builder()
+  end
+
+config.font_size = 22
+
+config.color_scheme = 'Catppuccin Macchiato'
+
+-- and finally, return the configuration to wezterm
+return config
